e41c9a5f827376155b86105d8d4737bb3fd2c159
Harsh <harsh.sumanpatel2003@gmail.com>
2022-11-13 11:16:03 +0530

    Initial commit

---
+++ app/src/main/java/com/example/madpractical6_21012022037/MainActivity.kt
@@ -0,0 +1,33 @@
+package com.example.madpractical6_21012022037
+
+import android.content.Intent
+import android.net.Uri
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.MediaController
+import android.widget.VideoView
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+import java.net.URI
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val myvideoview=findViewById<VideoView>(R.id.playview)
+        val mybuttonview=findViewById<FloatingActionButton>(R.id.playbtn1)
+
+        val mediaController=MediaController(this)
+        val uri:Uri=Uri.parse(("android.resource://"+packageName+"/"+R.raw.thestoryoflight))
+        myvideoview.setMediaController((mediaController))
+        mediaController.setAnchorView((myvideoview))
+        myvideoview.setVideoURI(uri)
+        myvideoview.requestFocus()
+        myvideoview.start()
+
+    mybuttonview.setOnClickListener{
+        Intent(this,YouTubeActivity::class.java).apply {
+            startActivity(this)
+        }
+    }
+    }
+}
